15e6f86008bea8dc66d3688c3c5699fa2781aaba
Byron J. Williams <byron.w@ieee.org>
2018-06-01 09:33:38 -0500

    added custom.el to config - breaks fork but ok

---
+++ personal/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(face-font-family-alternatives
+   (quote
+    (("Source Code Pro 15")
+     ("Monospace" "courier" "fixed")
+     ("Monospace Serif" "Courier 10 Pitch" "Consolas" "Courier Std" "FreeMono" "Nimbus Mono L" "courier" "fixed")
+     ("courier" "CMU Typewriter Text" "fixed")
+     ("Sans Serif" "helv" "helvetica" "arial" "fixed")
+     ("helv" "helvetica" "arial" "fixed"))))
+ '(package-selected-packages
+   (quote
+    (golden-ratio-scroll-screen
+     (quote golden-ratio-scroll-screen)
+     ledger-mode go-eldoc go-guru go-rename go-mode counsel-projectile ag zop-to-char zenburn-theme yaml-mode which-key web-mode volatile-highlights undo-tree smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters operate-on-number move-text monokai-theme molokai-theme magit key-chord json-mode js2-mode imenu-anywhere hl-todo helm-projectile helm-descbinds helm-ag guru-mode grizzl gotest god-mode go-projectile gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill discover-my-major diminish diff-hl crux counsel company-go company-auctex company-anaconda cdlatex browse-kill-ring beacon anzu ace-window)))
+ '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#1B1D1E" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 160 :width normal :foundry "nil" :family "Operator Mono")))))
