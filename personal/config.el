3a0aa210e85fbcb4c0e3b9123498be1e1d06ab45
Byron J. Williams <byron.w@ieee.org>
2018-06-05 20:29:06 -0500

    updated config

--- personal/config.el
+++ personal/config.el
@@ -37,19 +37,20 @@
 (defalias 'projectile-find-file 'counsel-projectile-find-file)
 (defalias 'helm-projectile-find-file 'counsel-projectile-find-file)
 
-(org-bullets-mode +1)
 (smartparens-strict-mode -1)
-(whitespace-mode -1)
 
 ;; (server-start)
 
 ;; Global Key Configs
 (global-set-key (kbd "C-q") 'emacs-surround)
+(global-set-key (kbd "C-r") 'isearch-forward)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 (global-set-key (kbd "C-x C-k k") 'kill-buffer)
 (global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
 (global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up)
 (global-set-key (kbd "C-x C-m") 'bjw/duplicate-line)
+(global-set-key (kbd "s-<up>") 'bjw/move-line-up)
+(global-set-key (kbd "s-<down>") 'bjw/move-line-down)
 
 (key-chord-define-global "kk" 'kill-whole-line)
 
