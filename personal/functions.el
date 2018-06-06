db7c6da3f4c010faee22aa8e10c9abecf694fc34
Byron J. Williams <byron.w@ieee.org>
2018-06-05 20:28:57 -0500

    move line functions added

--- personal/functions.el
+++ personal/functions.el
@@ -1,6 +1,30 @@
 ;;; functions
 
+;; Move line functions
+(defun bjw/move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
 
+(defun bjw/move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (bjw/move-line (if (null n) -1 (- n))))
+
+(defun bjw/move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (bjw/move-line (if (null n) 1 n)))
+
+;; duplicate line function
 (defun bjw/duplicate-line (arg)
   "Duplicate current line, leaving point in lower line."
   (interactive "*p")
@@ -35,3 +59,7 @@
 
   ;; put the point in the lowest line and return
   (next-line arg))
+
+;; Global Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
