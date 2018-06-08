081296754c45cd6d3fab72f944341af4140c8aa5
Byron J. Williams <me@drbyron.io>
2018-06-08 16:41:54 -0500

    config changes

--- personal/config.el
+++ personal/config.el
@@ -8,6 +8,7 @@
 (require 'prelude-go)
 (require 'prelude-c)
 (require 'prelude-css)
+(require 'prelude-scss)
 (require 'prelude-emacs-lisp)
 (require 'prelude-js)
 (require 'prelude-latex)
@@ -19,6 +20,7 @@
 ;; predule modules - utilities
 (require 'prelude-org)
 (require 'prelude-company)
+;; (require 'prelude-ido)
 (require 'prelude-helm-everywhere)
 (require 'prelude-helm)
 (require 'prelude-key-chord)
@@ -33,12 +35,16 @@
 (prelude-require-package 'golden-ratio-scroll-screen)
 (prelude-require-package 'org-bullets)
 (prelude-require-package 'org-autolist)
+;; (prelude-require-package 'doom-themes)
+(prelude-require-package 'all-the-icons)
+(prelude-require-package 'neotree)
+(prelude-require-package 'rainbow-delimiters)
 
 (defalias 'projectile-find-file 'counsel-projectile-find-file)
 (defalias 'helm-projectile-find-file 'counsel-projectile-find-file)
 
 (setq prelude-whitespace -1)
-
+(rainbow-delimiters-mode +1)
 (smartparens-strict-mode -1)
 
 ;; (server-start)
@@ -55,6 +61,23 @@
 (global-set-key (kbd "s-<down>") 'bjw/move-line-down)
 
 (key-chord-define-global "kk" 'kill-whole-line)
+
+;; Doom-Themes settings
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-one t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Enable custom neotree theme
+(doom-themes-neotree-config)  ; all-the-icons fonts must be installed!
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
 
 ;; Global Variables:
 ;; byte-compile-warnings: (not free-vars)
