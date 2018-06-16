57c78a45f96ad67de9c7b86a2ec576abb530a3cf
Byron J Williams <byron.w@ieee.org>
2018-06-16 09:23:36 -0500

    updated org mode settings

--- personal/bjw-org.el
+++ personal/bjw-org.el
@@ -9,9 +9,30 @@
   (setq tab-width 2)
 
   (org-autolist-mode +1)
+  (org-indent-mode +1)
+
+  (setq org-use-speed-commands t
+        org-hide-emphasis-markers t
+        org-src-fontify-natively t   ;; Pretty code blocks
+        org-pretty-entities t
+        org-src-tab-acts-natively t
+        org-completion-use-ido t
+        org-outline-path-complete-in-steps nil
+        org-confirm-babel-evaluate nil)
+
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (latex . t)
+     (ledger . t)
+     (python . t)
+     (sh . t)
+     (shell . t)
+     (sql . nil)))
+
   )
 
-(add-hook 'go-mode-hook 'bjw/my-org-mode-hook)
+(add-hook 'org-mode-hook 'bjw/my-org-mode-hook)
 
 ;; https://emacs.stackexchange.com/questions/19506/suppress-warning-assignment-to-free-variable-and-others
 ;; Local Variables:
