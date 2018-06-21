f034e6b20b256eb36facd8f4474a0a710e8f757c
Byron J Williams <byron.w@ieee.org>
2018-06-21 15:18:55 -0700

    updated org settings TODO and journal

--- personal/bjw-org.el
+++ personal/bjw-org.el
@@ -18,14 +18,46 @@
         org-pretty-entities t
         org-src-tab-acts-natively t
         org-completion-use-ido t
+        org-return-follows-link t
         org-outline-path-complete-in-steps nil
-        org-confirm-babel-evaluate nil)
+        org-confirm-babel-evaluate nil
+        org-todo-keywords '((sequence "TODO(t)" "IN PROGRESS(g)" "WAITING(w)" "|" "DONE(d)")
+                            (sequence "|" "CANCELED(c)")))
+
+  (setq org-journal-dir "~/journal/")
+  (setq org-journal-date-format "#+TITLE: Journal Entry- %e %b %Y (%A)")
+
+  (add-hook 'org-mode-hook 'yas-minor-mode-on)
+
+  (setq org-agenda-files '("~/Dropbox/org"
+                           "~/Dropbox/org/research"
+                           "~/Dropbox/org/spiritual"))
+
+  (font-lock-add-keywords            ; A bit silly but my headers are now
+   'org-mode `(("^\\*+ \\(TODO\\) "  ; shorter, and that is nice canceled
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "TODO ⚑")
+                          nil)))
+               ("^\\*+ \\(DOING\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "IN PROGRESS ⚐")
+                          nil)))
+               ("^\\*+ \\(CANCELED\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "✘")
+                          nil)))
+               ("^\\*+ \\(DONE\\) "
+                (1 (progn (compose-region (match-beginning 1) (match-end 1) "DONE ✔")
+                          nil)))))
 
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((emacs-lisp . t)
      (latex . t)
      (ledger . t)
+     (js . t)
+     (scala . t)
+     (clojure . t)
+     (go . t)
+     (bash . t)
+     (css . t)
      (python . t)
      (sh . t)
      (shell . t)
